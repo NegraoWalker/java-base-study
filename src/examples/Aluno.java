75091d68ede0cc9b402894f67cec7b3955f7a150
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-05-18 21:49:22 -0300

    Atualização versão java

--- src/examples/Aluno.java
+++ src/examples/Aluno.java
@@ -1,4 +1,90 @@
 package examples;
 
 public class Aluno {
+    private String nome;
+    private int idade;
+    private String dataNascimento;
+    private String cpf;
+    private String dataMatricula;
+    private double nota1;
+    private double nota2;
+    private double nota3;
+
+    public Aluno() {
+    }
+
+    public Aluno(String nome, int idade, String dataNascimento, String cpf, String dataMatricula, double nota1, double nota2, double nota3) {
+        this.nome = nome;
+        this.idade = idade;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+        this.dataMatricula = dataMatricula;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataMatricula() {
+        return dataMatricula;
+    }
+
+    public void setDataMatricula(String dataMatricula) {
+        this.dataMatricula = dataMatricula;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
 }
